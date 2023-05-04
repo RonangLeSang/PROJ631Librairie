8c7d40df41148f15f55f7f5e47b26417fc0495cc
ronanter <ronan1terras@gmail.com>
2023-05-04 18:05:00 +0200

    [FEATURE] Onglets fenetre et scrollingpane qui marche pas

--- Java/Main.java
+++ Java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test 2");
+        System.out.println("test");
 
         MainWindow window = new MainWindow();
     }
