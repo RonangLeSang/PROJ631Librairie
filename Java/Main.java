1162511a04329ae7fd869b7ad0cf201e0f286c58
loisblin <113303000+loisblin@users.noreply.github.com>
2023-05-04 15:58:06 +0200

    [TEST]

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test hyugfyuyfihig_iugh_ig_");
+        System.out.println("test hyugfyuyfihig_iugh_ig(jtyhyyt_");
     }
 }
