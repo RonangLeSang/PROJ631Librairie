d047605171690d59617e830992a8ac733b596c6f
Mouheb Sabri <122006215+MouhebSabri@users.noreply.github.com>
2023-05-04 17:29:54 +0200

    Update Book.java

--- Java/Book.java
+++ Java/Book.java
@@ -1,108 +1,108 @@
 import java.util.Date;
 
 public class Book {
-  private int id;
-  private String title;
-  private String author;
-  private String genre;
-  private String publisher;
-  private String image;
-  private Date date;
-  private int pageCount;
-  private String summary;
-  private String language;
+  private int Id;
+  private String Title;
+  private String Author;
+  private String Genre;
+  private String Publisher;
+  private String Image;
+  private Date Date;
+  private int PageCount;
+  private String Summary;
+  private String Language;
 
   public Book(int id, String title, String author, String genre, String publisher, String image, Date date, int pageCount, String summary, String language) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.genre = genre;
-    this.publisher = publisher;
-    this.image = image;
-    this.date = date;
-    this.pageCount = pageCount;
-    this.summary = summary;
-    this.language = language;
+    this.Id = id;
+    this.Title = title;
+    this.Author = author;
+    this.Genre = genre;
+    this.Publisher = publisher;
+    this.Image = image;
+    this.Date = date;
+    this.PageCount = pageCount;
+    this.Summary = summary;
+    this.Language = language;
   }
 
   // Getters and Setters
   public int getId() {
-    return id;
+    return Id;
   }
 
   public void setId(int id) {
-    this.id = id;
+    this.Id = id;
   }
 
   public String getTitle() {
-    return title;
+    return Title;
   }
 
   public void setTitle(String title) {
-    this.title = title;
+    this.Title = title;
   }
 
   public String getAuthor() {
-    return author;
+    return Author;
   }
 
   public void setAuthor(String author) {
-    this.author = author;
+    this.Author = author;
   }
 
   public String getGenre() {
-    return genre;
+    return Genre;
   }
 
   public void setGenre(String genre) {
-    this.genre = genre;
+    this.Genre = genre;
   }
 
   public String getPublisher() {
-    return publisher;
+    return Publisher;
   }
 
   public void setPublisher(String publisher) {
-    this.publisher = publisher;
+    this.Publisher = publisher;
   }
 
   public String getImage() {
-    return image;
+    return Image;
   }
 
   public void setImage(String image) {
-    this.image = image;
+    this.Image = image;
   }
 
   public Date getDate() {
-    return date;
+    return Date;
   }
 
   public void setDate(Date date) {
-    this.date = date;
+    this.Date = date;
   }
 
   public int getPageCount() {
-    return pageCount;
+    return PageCount;
   }
 
   public void setPageCount(int pageCount) {
-    this.pageCount = pageCount;
+    this.PageCount = pageCount;
   }
 
   public String getSummary() {
-    return summary;
+    return Summary;
   }
 
   public void setSummary(String summary) {
-    this.summary = summary;
+    this.Summary = summary;
   }
 
   public String getLanguage() {
-    return language;
+    return Language;
   }
 
   public void setLanguage(String language) {
-    this.language = language;
+    this.Language = language;
   }
 }
