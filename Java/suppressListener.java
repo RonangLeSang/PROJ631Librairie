e7de647839606c86532af91ddc5ee7ddebae8a59
ronanter <ronan1terras@gmail.com>
2023-05-07 14:05:46 +0200

    [FIX] GitHub qui marchait plus

--- Java/suppressListener.java
+++ Java/suppressListener.java
@@ -1,7 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class supressListener implements ActionListener {
+public class suppressListener implements ActionListener {
 
     private ModelWindow modelWindow;
     private Item item;
