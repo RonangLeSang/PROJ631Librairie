6aaf26eb00be40db82305f530e73eed0250ebb1e
ronanter <ronan1terras@gmail.com>
2023-05-11 08:39:37 +0200

    [FEATURE] popup ajout d'attributs

--- Java/AddButtonListener.java
+++ Java/AddButtonListener.java
@@ -14,40 +14,15 @@ public class AddButtonListener implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         switch (modelWindow.getTab()){
             case 1:
-                UserPopUp userPopUp = new UserPopUp();
-                String[] userFields = userPopUp.getFields();
-                User user = new User(userFields[0], userFields[3], java.sql.Date.valueOf(userFields[2]));
-                try {
-                    modelWindow.executeRequest(user.toSQL());
-                } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
-                }
+                UserPopUp userPopUp = new UserPopUp(modelWindow);
                 break;
 
             case 2:
-                BookPopUp bookPopUp = new BookPopUp();
-                String[] bookFields = bookPopUp.getFields();
-                Book book = new Book(Integer.parseInt(bookFields[0]), bookFields[1], bookFields[2], bookFields[3],
-                        bookFields[4], bookFields[5], java.sql.Date.valueOf(bookFields[6]),
-                        Integer.parseInt(bookFields[7]), bookFields[8], bookFields[9]);
-                try {
-                    modelWindow.executeRequest(book.toSQL());
-                } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
-                }
+                BookPopUp bookPopUp = new BookPopUp(modelWindow);
                 break;
 
             case 3:
-                AdvicePopUp advicePopUp = new AdvicePopUp();
-                String[] adviceFields = advicePopUp.getFields();
-                Advice advice = new Advice(Integer.parseInt(adviceFields[0]), Integer.parseInt(adviceFields[2]),
-                        java.sql.Date.valueOf(adviceFields[3]), Integer.parseInt(adviceFields[4]), adviceFields[5],
-                        adviceFields[6], adviceFields[7]);
-                try {
-                    modelWindow.executeRequest(advice.toSQL());
-                } catch (SQLException ex) {
-                    throw new RuntimeException(ex);
-                }
+                AdvicePopUp advicePopUp = new AdvicePopUp(modelWindow);
                 break;
         }
     }
