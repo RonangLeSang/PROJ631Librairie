1400f7e98b9e4e5e285e3d65db2b9f6c2f5eb8b4
Bob <113138769+chafikya@users.noreply.github.com>
2023-05-11 11:07:58 +0200

    [FEATURES]inscription

---
+++ HTML_PHP_CSS/register.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Connectez-vous à votre base de données
+    $conn = mysqli_connect("tp-epua:3308", "tafarou", "rt45y8at");
+    mysqli_select_db($conn, "tafarou");
+
+    // Vérifiez si le nom d'utilisateur existe déjà dans la base de données
+    $sql = "SELECT * FROM utilisateur WHERE login = '$username'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        echo "Nom d'utilisateur déjà utilisé. Veuillez choisir un nom d'utilisateur différent.";
+    } else {
+        // Insérez les données de l'utilisateur dans la table "utilisateur"
+        $sql = "INSERT INTO utilisateur (login, MDP) VALUES ('$username', '$password')";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            // Inscription réussie, affichez le message et redirigez l'utilisateur après 5 secondes
+            echo "Inscription réussie. Vous allez être redirigé vers la page d'accueil.";
+            echo "<meta http-equiv='refresh' content='5;URL=accueil.php'>";
+        } else {
+            echo "Une erreur s'est produite lors de l'inscription. Veuillez réessayer.";
+        }
+    }
+}
+?>
+
