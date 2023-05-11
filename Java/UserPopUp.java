1b15a1ff8af85e57126208282675372eb97020a9
loisblin <113303000+loisblin@users.noreply.github.com>
2023-05-11 08:49:00 +0200

    [FEATURES] class userPopUP (je finirai plus tard  )

--- Java/UserPopUp.java
+++ Java/UserPopUp.java
@@ -9,42 +9,59 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+
 public class UserPopUp extends PopUp implements ActionListener{
-    private User user;
-    public void displayFields() {
+    private ModelWindow modelWindow;
+    private JTextField login =new JTextField();
+    private JTextField passeword = new JTextField();
+    private String sql;
+
+
+    public void displayFields(ModelWindow modelWindow) {
+        super(modelWindow);
+        setSize(1000, 600);
 
         JButton valider=new JButton("valider");
         JButton annuler=new JButton("annuler");
         JTextField login = new JTextField(10);
         JTextField passeword = new JTextField(10);
-        SpinnerDateModel dateModel = new SpinnerDateModel(new Date(), null, null, Calendar.DAY_OF_MONTH);
-        JSpinner spinner = new JSpinner(dateModel);
+
 
         JPanel panel = new JPanel();
         panel.add(new JLabel("login :"));
         panel.add(login);
         panel.add(new JLabel("MDP :"));
         panel.add(passeword);
-        panel.add(new JLabel("Date :"));
-        panel.add(spinner);
 
         valider.addActionListener(this);
         valider.setActionCommand("valider");
 
         annuler.addActionListener(this);
         annuler.setActionCommand("annuler");
-        String loginText = login.getText();
-        String passewordText = passeword.getText();
-        Date selectedDate = (Date) dateModel.getValue();
 
-        }
+        this.setVisible(true);
+
+    }
     public void actionPerformed(ActionEvent e) {
-        if (1==1) {
+        if(e.getActionCommand().equals("valider")){
+            String loginText=login.getText();
+            String passewordText=passeword.getText();
+            java.util.Date utilDate = new java.util.Date();
+            java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+            User user=new User(loginText,passewordText,sqlDate);
+            try {
+                modelWindow.executeRequest(user.toSQL());
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+
 
         }
-    }
-    @Override
-    public String[] getFields() {
-        return new String[0];
-    }
+
+
+
+        }
+
+
 }
