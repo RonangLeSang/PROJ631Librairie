bc9e304bd1b8ac68558aa76b9b1ef3dbe1ad053c
loisblin <113303000+loisblin@users.noreply.github.com>
2023-05-11 09:59:25 +0200

    [FEATURES] class userPopUP (je finirai plus tard  )

--- Java/AdvicePopUp.java
+++ Java/AdvicePopUp.java
@@ -1,3 +1,15 @@
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.Calendar;
+import java.util.Date;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class AdvicePopUp extends PopUp{
     public AdvicePopUp(ModelWindow modelWindow) {
         super(modelWindow);
